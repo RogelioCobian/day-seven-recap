514ed4afa4ac475ee2c78eda79d1229224e2b9bf
Rogelio Cobian <rogeliocobian2@gmail.com>
2018-06-07 14:35:29 -0700

    added components

---
+++ src/app/message/message.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // selector: 'message-component',
+  // selector: '[message-component]',
+  selector: '.message-component',
+  templateUrl: './message.component.html',
+  styleUrls: ['./message.component.css']
+})
+
+export class MessageComponent {
+
+}
