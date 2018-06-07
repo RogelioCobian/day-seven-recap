ce92cd039410b490e422d0a99f86ed98fd1b89fc
Rogelio Cobian <rogeliocobian2@gmail.com>
2018-06-07 13:09:44 -0700

    added classes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+class CarBrand {
+  constructor(protected make: string ) {}
+}
+
+class Car extends CarBrand {
+  // constructor() {}
+  constructor(public make: string, public model: string, public year: number ) {
+    super(make);
+  }
+  getDetails() {
+    return(this.make = `${this.make}` , this.model = `${this.model}`, this.year = this.year);
+  }
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  ford: CarBrand = new CarBrand('Ford');
+  honda: Car = new Car('Honda', 'Civic', 1996 );
+
+  ngOnInit() {
+    console.log(this.ford);
+    console.log(this.honda.make);
+  }
+
 }
