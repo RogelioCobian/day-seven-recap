0dfab1d7982837c1be3aaeb607a6f17127ffe404
Rogelio Cobian <rogeliocobian2@gmail.com>
2018-06-07 13:42:05 -0700

    added interfaces

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface Fighter {
+  name: string;
+  number: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  fighter1: Fighter = {name: 'Jim', number: 10};
+  fighter2: Fighter = {name: 'Freddy', number: 20};
+
+  ngOnInit() {
+    console.log(this.fighter1, this.fighter2);
+  }
 }
